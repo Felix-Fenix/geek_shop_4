f2f779d1748d82a812f93ca111a98cef8664dc97
Felix-Fenix <happyhuman@bk.ru>
2022-08-27 18:53:03 +0700

    27.08.22/8

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 export EMAIL_HOST_USER="merkava2016@bk.ru"
 export EMAIL_HOST_PASSWORD="9SkY6Vrt36SFMgPsdBvU"
 export DJANGO_PRODUCTION=1
-export DOMAIN="10.8.0.46"
+export DOMAIN="http://10.8.0.46"
 export CLIENT_ID="5c0bf55af4365cc1606c"
 export CLIENT_SECRET="c2df30fee0eeb6251b5c1e50ca4064a932895fa9"
 
@@ -11,11 +11,11 @@ sudo apt update && sudo apt upgrade -y
 
 #sudo apt --fix-broken install -y
 #useradd -g www-data -m django
-sudo groupadd www-data2
+sudo groupadd www-data
 sudo apt install git
 git clone https://github.com/Felix-Fenix/geek_shop_4.git -b lesson_14
 
-sudo chown -R fenix:www-data2 /home/fenix/geek_shop_4
+sudo chown -R fenix:www-data /home/fenix/geek_shop_4
 sudo apt install -y python3-venv
 python3 -m venv /home/fenix/venv
 source venv/bin/activate
@@ -29,12 +29,12 @@ sudo systemctl start gunicorn
 sudo systemctl status gunicorn
 
 sudo apt install -y nginx
-cp /home/fenix/geek_shop_4/geekshop_nginx /etc/nginx/sites-available/
+sudo cp /home/fenix/geek_shop_4/geekshop_nginx /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/geekshop_nginx /etc/nginx/sites-enabled/geekshop_nginx
 # Перезапускаем nginx
 sudo systemctl restart nginx
 sudo systemctl status nginx
 
 #export DOMAIN="https://test-nginx.vpnforall.su"
-python manage.py migrate
+python /home/fenix/geek_shop_4/manage.py migrate
 reboot

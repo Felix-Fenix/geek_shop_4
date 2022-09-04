60053ff5a40323ad8faa34e0b05d239e56e6458e
Felix-Fenix <happyhuman@bk.ru>
2022-09-04 19:50:06 +0700

    4.09.22/2

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -89,7 +89,14 @@ if not DEBUG:
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
-# else:
+
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 #     DATABASES = {
 #         "default": {
 #             "NAME": "geekshop",
